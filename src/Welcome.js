f38792a1f5b0f20f766a2ca20c519bd548a23a44
xiaokunxu <996670910@qq.com>
2017-04-14 14:14:32 +0800

    state

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,14 +5,29 @@ import React from 'react';
 //     }
 // }
 //  传递父件参数（props）
-// class Welcome extends React.Component {
-//     render() {
-//         return <h1 > Hello, { this.props.name } < /h1>;
-//     }
-// }
-//  等价于
-function Welcome(props) {
-    return <h1 > Hello, { props.name } < /h1>;
-}
+class Welcome extends React.Component {
+        constructor(props) {
+            super(props);
+            this.state = { date: new Date() };
+            setInterval(() => {
+                this.state = {
+                    date: new Date()
+                }
+            })
+        }
+        render() {
+            return ( <
+                div >
+                <
+                h1 > Hello, { this.props.name } < /h1> <
+                h2 > { this.state.date.toString() } < /h2> < /
+                div >
+            )
+        }
+    }
+    //  等价于
+    // function Welcome(props) {
+    //     return <h1 > Hello, { props.name } < /h1>;
+    // }
 
 export default Welcome
