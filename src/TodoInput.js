87a1de0f441137acc103d5da4930d018d20a95b6
xiaokunxu <996670910@qq.com>
2017-04-17 16:26:28 +0800

    监听回车事件

--- src/TodoInput.js
+++ src/TodoInput.js
@@ -4,6 +4,12 @@ export default class TodoInput extends Component {
     render() {
         return <input type = "text"
         defaultValue = { this.props.content }
+        onKeyPress = { this.submit }
         />
+    }
+    submit(e) {
+        if (e.key === 'Enter') {
+            console.log('用户按回车了')
+        }
     }
 }
